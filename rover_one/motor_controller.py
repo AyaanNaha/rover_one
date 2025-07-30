4110caf912d36ba67fc736a36cdbef419d7de1a8
AyaanNaha <ayaan.naha26@gmail.com>
2025-07-30 14:28:57 -0700

    FIXINGTHESERIALCOMMS

--- rover_one/motor_controller.py
+++ rover_one/motor_controller.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Twist
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1.0)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1.0)
 time.sleep(3)
 ser.reset_input_buffer()
 print("serial ok")
